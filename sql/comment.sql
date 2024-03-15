0972a70c1a996acbbe7ceead703256930716dc28
hotpocket <githotpocket@gmail.com>
2024-03-15 13:38:33 -0700

    remove wrong comments. make explicit the cascade (added user_id)

--- sql/comment.sql
+++ sql/comment.sql
@@ -3,8 +3,6 @@ CREATE TABLE comment (
   post_id INTEGER NOT NULL,
   user_id INTEGER NOT NULL,
   comment_text text,
-  -- when a post is deleted, delete all comments associated with that post
   CONSTRAINT post_fk FOREIGN KEY (post_id) REFERENCES post (id) ON DELETE CASCADE,
-  -- but when a user is deleted, keep the comments (no DELETE CASCADE)
-  CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES user (id)
+  CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE
 );

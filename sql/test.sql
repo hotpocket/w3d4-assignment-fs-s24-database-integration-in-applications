0fa1af7c1b896e28b8229fb95ea143ac51d9494c
hotpocket <githotpocket@gmail.com>
2024-03-15 13:43:12 -0700

    more concise wording/readability of test

--- sql/test.sql
+++ sql/test.sql
@@ -27,9 +27,9 @@ SELECT "Inserting data";
 INSERT INTO user (username, password) VALUES ("admin5", "$uper$ecret0MG!");
 INSERT INTO user (username, password) VALUES ("user7", "$uper$ecret5auce");
 INSERT INTO post (user_id, post_text) VALUES (1, "post1(admin5) - Eye NO th!ngs, truest meee!");
-INSERT INTO post (user_id, post_text) VALUES (2, "post1(user7) - I'm the best!  I do stuff!");
-INSERT INTO comment (post_id, user_id, comment_text) VALUES (1,2, "comment1 - I don't believe your lies admin5");
-INSERT INTO comment (post_id, user_id, comment_text) VALUES (2,1, "comment2 - You're a noob user7!");
+INSERT INTO post (user_id, post_text) VALUES (2, "post2(user7) - I'm the best!  I do stuff!");
+INSERT INTO comment (post_id, user_id, comment_text) VALUES (1,2, "user7 comment on post 1 - I don't believe your lies admin5");
+INSERT INTO comment (post_id, user_id, comment_text) VALUES (2,1, "admin5 comment on post 2 - You're a noob user7!");
 
 
 SELECT "INSERTs done:";
